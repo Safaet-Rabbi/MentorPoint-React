7670b8a763c510492003c807b60de37456d454dc
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-12-22 20:38:08 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import NavbarBanner from "./components/Navbar/NavbarBanner";
+import Hero from "./components/Hero/Hero";
+import NumberCounter from "./components/NumberCounter/NumberCounter";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import Img1 from "./assets/banner1.png";
+import Img2 from "./assets/banner2.png";
+import Banner from "./components/Banner/Banner";
+import SubjectCard from "./components/SubjectCard/SubjectCard";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Footer from "./components/Footer/Footer";
+
+const BannerData = {
+  image: Img1,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Personalized Professional Online Tutor on Your Schedule",
+  subtitle:
+    "At Mentor Point, we pride ourselves on connecting students with the most talented and qualified tutors who are dedicated to helping you achieve your academic goals. Our tutors are carefully selected based on their expertise, teaching experience, and passion for empowering learners.",
+  link: "#",
+};
+
+const BannerData2 = {
+  image: Img2,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Talented and Qualified Tutors to Serve You for Help",
+  subtitle:
+    "From mastering complex concepts to gaining confidence in your abilities, we’re committed to helping you succeed at every step of your educational journey.",
+  link: "#",
+};
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden">
+      <Navbar />
+      <NavbarBanner />
+      <Hero />
+      <NumberCounter />
+      <WhyChooseUs />
+      <Banner {...BannerData} />
+      <Banner {...BannerData2} reverse={true} />
+      <SubjectCard />
+      <Testimonial />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
